75d1c2972f2ca7a2712070bb4cbdc2fd33405123
javierjsv <javier.solis@novaip.com.co>
2019-01-17 09:39:24 -0500

    usando Renderer2

--- src/app/pages/hobies/hobies.component.ts
+++ src/app/pages/hobies/hobies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
 @Component({
@@ -10,15 +10,23 @@ export class HobiesComponent implements OnInit {
   mostrar: boolean;
   hobby: string[];
   tab: number;
+  title: string;
 
-  constructor() {
+
+  //Renderer2 escucha elementos del dom 
+  constructor(private renderer2: Renderer2) {
 
     this.mostrar = false;
     this.hobby = ['correr', 'leer', 'cantar'];
     this.tab = 1;
+    this.title = 'ejemplo con ngSwitch';
 
   }
 
+
+  ngOnInit() {
+    this.usandoRenderer2();
+  }
 
 
 
@@ -28,13 +36,20 @@ export class HobiesComponent implements OnInit {
 
   }
 
+  usandoRenderer2() {
+    this.renderer2.listen('document', 'click', (e) => {
+      console.log('posición X', e.clientX);
+      console.log('posición Y', e.clientY);
+    });
+  }
+
 
   formulary(datos) {
 
     console.log(datos.value);
 
 
-    if (datos.value == '') {
+    if (datos.value === '') {
 
       alert('no puede estar vacio');
       datos.valu = !null;
@@ -49,8 +64,5 @@ export class HobiesComponent implements OnInit {
 
   }
 
-
-  ngOnInit() {
-  }
 
 }
