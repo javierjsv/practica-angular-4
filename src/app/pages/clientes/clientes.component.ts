f4be045e0fc442daad9a6deeb5927b22cc95a705
javierjsv <javier.solis@novaip.com.co>
2019-01-18 15:53:53 -0500

    cambio de .css por scss

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -5,111 +5,103 @@ import { FormsModule } from '@angular/forms';
 
 
 @Component({
-	selector: 'app-clientes',
-	templateUrl: './clientes.component.html',
-	styleUrls: ['./clientes.component.css']
+selector: 'app-clientes',
+templateUrl: './clientes.component.html',
+styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
 
-	Empleados = [
-		{ 'name': 'Camilo', apellidos: 'Velasco', porcentaje: 0.5827, activo: true },
-		{ 'name': 'Marielas', apellidos: 'Solis', porcentaje: 0.2527, activo: false },
-		{ 'name': 'Marisol', apellidos: 'Solis', porcentaje: 0.2527, activo: true },
-		{ 'name': 'Iker', apellidos: 'Gossen', porcentaje: 0.927, activo: true }
-	];
+Empleados = [
+{ 'name': 'Camilo', apellidos: 'Velasco', porcentaje: 0.5827, activo: true },
+{ 'name': 'Marielas', apellidos: 'Solis', porcentaje: 0.2527, activo: false },
+{ 'name': 'Marisol', apellidos: 'Solis', porcentaje: 0.2527, activo: true },
+{ 'name': 'Iker', apellidos: 'Gossen', porcentaje: 0.927, activo: true }
+];
 
-	model: any = {};
-	model2: any = {};
-	myValue; //variable Global
-	mjs = '';
-	mensaje = false;
-	mensajes = false;
-	ocultarActu: boolean = true;
-
-
-	constructor() {
-
-	}
+model: any = {};
+model2: any = {};
+myValue; // variable Global
+mjs = '';
+mensaje = false;
+mensajes = false;
+ocultarActu: boolean = true;
 
 
-	// agregar metos 
+constructor() {
+
+}
 
 
-	createEmpleado(): void {
+// agregar metos
 
 
-		if (this.model == null) {
-			alert('No se encontraron datos');
-		}
-		else {
-
-			this.Empleados.push(this.model);
-			console.log(this.model);
-			this.mensaje = true;
-			this.mjs = 'Cliente creado';
-			this.model.name = '';
-			this.model.apellidos = '';
-		}
+createEmpleado(): void {
 
 
+if (this.model == null) {
+alert('No se encontraron datos');
+} else {
+ this.Empleados.push(this.model);
+ console.log(this.model);
+ this.mensaje = true;
+ this.mjs = 'Cliente creado';
+ this.model.name = '';
+ this.model.apellidos = '';
+}
+}
 
 
-	}
+deleEmpleado(i): void {
+
+let answer = confirm('Desea eliminar');
+if (answer) {
+this.Empleados.splice(i, 1);
+this.mensaje = true;
+this.mjs = 'Borrado';
+}
+}
 
 
-	deleEmpleado(i): void {
-
-		var answer = confirm('Desea eliminar');
-		if (answer) {
-			this.Empleados.splice(i, 1);
-
-
-			this.mensaje = true;
-			this.mjs = 'Borrado';
-		}
-	}
+editEmpleado(i): void {
+this.ocultarActu = false;
+this.model2.name = this.Empleados[i].name;
+this.model2.apellidos = this.Empleados[i].apellidos;
+this.model2.porcentaje = this.Empleados[i].porcentaje;
+this.myValue = i;
+}
 
 
-	editEmpleado(i): void {
-		this.ocultarActu = false;
-		this.model2.name = this.Empleados[i].name;
-		this.model2.apellidos = this.Empleados[i].apellidos;
-		this.model2.porcentaje = this.Empleados[i].porcentaje;
-		this.myValue = i;
-	}
+updateEmpleado(): void {
 
+this.ocultarActu = true;
+this.mjs = 'actualizado';
+if (this.myValue == null) {
 
-	updateEmpleado(): void {
-
-		this.ocultarActu = true;
-		this.mjs = 'actualizado';
-		if (this.myValue == null) {
-
-			alert("no puedes estar vacio");
-		} else {
-			let i = this.myValue;
-			for (let j = 0; j < this.Empleados.length; j++) {
-				if (i == j) {
-					this.Empleados[i] = this.model2;
-					// this.msg = 'campo actualizado';
-					this.model2 = {};
-				}
-			}
-		}
+alert("no puedes estar vacio");
+} else {
+let i = this.myValue;
+for (let j = 0; j < this.Empleados.length; j++) {
+if (i == j) {
+this.Empleados[i] = this.model2;
+// this.msg = 'campo actualizado';
+this.model2 = {};
+}
+}
+}
 
 
 
 
 
 
-	}
+}
 
-	ngOnInit() {
+ngOnInit() {
 
-		this.Empleados.forEach(function (value) {
-			console.table(value);
-		});
-	}
+this.Empleados.forEach(function (value) {
+console.table(value);
+});
+}
 
 
 
